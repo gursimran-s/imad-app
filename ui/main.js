eb85d31f7da667b21d42d9db27c67bca5a2ee370
gursimran-s <31366097+gursimran-s@users.noreply.github.com>
2017-08-26 16:38:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //Move the image
-var img= document.getElementById('img');
+var img= document.getElementById('madi');
 img.onclick= function() {
     img.style.marginLeft= '100px';
 };
